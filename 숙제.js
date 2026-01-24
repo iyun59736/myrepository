24881623747c53ec04b36318e3223b1a69f3ddbe
iyun59736 <iyun59736@gmail.com>
2026-01-24 21:48:35 +0900

    Update and rename 문제양식.txt to 숙제.js

---
+++ 숙제.js
@@ -0,0 +1,66 @@
+quizFiles.push({
+  name: "JavaScriptTestFile의 복사본",
+  data: [
+
+    // 1번 문제
+    {
+      question: "바르게 영작한 것은? : 나는 영어를 배우는 중이다.",
+      correct: "I am learning English.",
+      wrong1: "I are learning English.",
+      wrong2: "I am learned English.",
+      wrong3: "I are learned English.",
+      explanation: "[배우다]인 learn을 [배우는 중인]이라는 능동 분사로 만들기 위해 ing를 붙인다."
+    },
+
+    // 2번 문제
+    {
+      question: "바르게 영작한 것은? : 그는 학생이고, 그녀는 선생이다.",
+      correct: "He is a student, and she is a teacher.",
+      wrong1: "He is a student, but she is a teacher.",
+      wrong2: "He is a student, so she is a teacher.",
+      wrong3: "He is a student, though she is a teacher.",
+      explanation: "[그리고]를 뜻하는 접속사는 and다."
+    },
+
+    // 3번 문제
+    {
+      question: "다음 중 뜻이 다른 하나를 고르시오.",
+      correct: "while",
+      wrong1: "but",
+      wrong2: "however",
+      wrong3: "yet",
+      explanation: "while은 [반면에], 나머지는 [그러나]다."
+    },
+
+    // 4번 문제
+    {
+      question: "나는 책을 앍을 것이다. : I ___ read a book.",
+      correct: "am going to",
+      wrong1: "must",
+      wrong2: "have to",
+      wrong3: "need to",
+      explanation: "나머지 선지들은 [해야 한다]라는 의미다."
+    },
+
+    // 5번 문제
+    {
+      question: "올바른 뜻을 고르시오. : Wake",
+      correct: "잠에서 깨다",
+      wrong1: "관찰하다",
+      wrong2: "잠에 들다",
+      wrong3: "물감을 칠하다",
+      explanation: "선지를 직접 만듦으로써 대조해서 찍기 어렵도록 유도함."
+    },
+
+    // 6번 문제
+    {
+      question: "[잠에서 깨다]를 고르시오.",
+      correct: "wake",
+      wrong1: "lake",
+      wrong2: "bake",
+      wrong3: "take",
+      explanation: "선지를 영어로 제시함으로써 난이도를 상승시킴."
+    }
+  ]
+});
+
